52165af392711df353f625575500143de43efa58
daudf27 <faotdaud27@gmail.com>
2023-08-15 23:55:02 +0800

    revisi part 2

--- cekph.php
+++ cekph.php
@@ -1,6 +1,6 @@
 <?php 
 	//koneksi untuk db
-	$konek = mysqli_connect("localhost", "flyupid_hydroponic", "hydPNC120823_", "flyupid_hydroponic"); 
+	$konek = mysqli_connect("localhost", "flyupid_hydroponic_2", "hydPNC120823_", "flyupid_hydroponic_2"); 
 
 
 	//baca data dari tabel tbhidroponik
