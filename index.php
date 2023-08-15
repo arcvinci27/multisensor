1dd6cc5f2647f4eb535ff4776111617133896197
daudf27 <faotdaud27@gmail.com>
2023-08-16 00:27:41 +0800

    tess2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Mwah Sinta Wally</title>
+    <title>Monitoring IOT Hidroponik</title>
 
     <script type="text/javascript" src="jquery/jquery.min.js"></script>
     <!--otomatis load/realtime-->
