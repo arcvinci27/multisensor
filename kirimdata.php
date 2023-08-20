1405327fd33bac5478ad23b96007760d0c16a951
daudf27 <faotdaud27@gmail.com>
2023-08-21 00:52:32 +0800

    perbaiki variabel aliran yang tertukar dengan ph

--- kirimdata.php
+++ kirimdata.php
@@ -13,7 +13,7 @@
 	mysqli_query($konek, "ALTER TABLE tbhidroponik AUTO_INCREMENT=1");
 
 	//simpan data ke tbhidroponik
-	$simpan = mysqli_query($konek, "update tbhidroponik set tds=$tds, ph=$ph, aliran=$ph where id=1");
+	$simpan = mysqli_query($konek, "update tbhidroponik set tds=$tds, ph=$ph, aliran=$aliran where id=1");
 
 	//uji var simpan untuk beri respon
 	if($simpan)
