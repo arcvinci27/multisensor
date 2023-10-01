633570d85918dd7237c325df931e169c82b26b41
arcvinci27 <faotdaud27@gmail.com>
2023-10-01 23:54:42 +0800

    add ceksuhu.php

---
+++ ceksuhu.php
@@ -0,0 +1,18 @@
+<?php 
+	//koneksi untuk db
+	include "koneksi.php";
+
+	//baca data dari tabel tbhidroponik
+	$sql = mysqli_query($konek, "select * from tbhidroponik order by id desc"); //data terakhir ada di atas db
+
+	//baca data dari atas
+	$data = mysqli_fetch_array($sql);
+	$suhu = $data['suhu'];
+
+	//cek apa data tds =0?
+	if( $suhu == "" ) $suhu = 0;
+
+	// cetak nilai
+	echo $suhu ;
+
+ ?>
